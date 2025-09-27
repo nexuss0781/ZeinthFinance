feb289317769f1b43f80957c221118eee083938d
* <nexuss0781@gmail.com>
2025-09-27 12:25:57 +0300

    Fix: Correct syntax error in App.jsx

--- dev/frontend/src/App.jsx
+++ dev/frontend/src/App.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import { Container } from '@mui/material';
-
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import { Container } from '@mui/material';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
-
 import TransactionsPage from './pages/TransactionsPage';
-
 import CategoriesPage from './pages/CategoriesPage';
-
 import ReportsPage from './pages/ReportsPage';
 
 function App() {
@@ -37,7 +29,5 @@ function App() {
     </Router>
   );
 }
-
-export default App;
 
 export default App;
